5805bbaa6811f75d0f46083664e271d8a5d93c98
alexfu2011 <alexfu2011@sina.com>
2020-11-12 17:27:51 +0800

    first commit

--- app/server/createHandle.js
+++ app/server/createHandle.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const querystring = require('querystring');
 
 const _handle = {}
 
@@ -8,13 +9,20 @@ function createHandle(pathname, handle) {
 
 function serve(req, resp) {
     const pathname = url.parse(req.url).pathname;
-
-    if (typeof _handle[pathname] == 'function') {
-        _handle[pathname](req, resp);
-    } else {
-        resp.writeHead(404, {'Content-Type': 'text/plain; charset=utf8'});
-        resp.end('该页不存在！');
-    }
+    let str = ''; 
+    req.on('data', function(thunk){
+        str += thunk;
+    });
+    req.on('end', function(){
+        if (typeof _handle[pathname] == 'function') {
+            req.form = querystring.parse(str);
+            req.query = url.parse(req.url, true).query;
+            _handle[pathname](req, resp);
+        } else {
+            resp.writeHead(404, {'Content-Type': 'text/plain; charset=utf8'});
+            resp.end('该页不存在！');
+        }
+    });
 }
 
 module.exports =  { createHandle, serve };
